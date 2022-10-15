97cc5bf6effbd0384bf25daeb4f0a9893566a8d1
Infinidoge <infinidoge@doge-inc.net>
2022-10-15 17:50:34 -0400

    port to 1.18.2

--- src/main/java/moe/inx/simple_durability_tooltip/mixin/ItemStackMixin.java
+++ src/main/java/moe/inx/simple_durability_tooltip/mixin/ItemStackMixin.java
@@ -14,6 +14,7 @@ import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
@@ -32,7 +33,7 @@ public abstract class ItemStackMixin {
 			CallbackInfoReturnable<List<Text>> ci, List<Text> list) {
 		if (!context.isAdvanced()) {
 			if (this.isDamaged()) {
-				list.add(Text.translatable("item.durability", this.getMaxDamage() - this.getDamage(),
+				list.add(new TranslatableText("item.durability", this.getMaxDamage() - this.getDamage(),
 						this.getMaxDamage()));
 			}
 		}
